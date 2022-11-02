5c8febda3b02cf900d4ed484f7d984d69baa8cc5
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-11-02 15:33:49 -0300

    validação para from de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //Model Credenciais para validar dados do nosso componente login
+  // Usando o  [(ngModel)] no html. fuciona mãodupla
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
 
-  constructor() { }
+  //Varaviel de controler de formulario feino no html com
+  //[fromControl="email"] 
+  //Usando required mostra o campo fica obrigatorio
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor () {}
 
   ngOnInit(): void {
   }
 
+  //Método para validar campos usando os dados email do new FromControll
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid
+  }
 }
