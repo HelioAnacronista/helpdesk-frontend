04a7455a937a9cdc26d921772a0c31f0ac2a9a78
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-11-09 10:27:21 -0300

    fix: Tecnico crud

--- src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -5,12 +5,13 @@ import { ToastrService } from 'ngx-toastr';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
+
 @Component({
-  selector: 'app-tecnicos-update',
-  templateUrl: './tecnicos-update.component.html',
-  styleUrls: ['./tecnicos-update.component.css']
+  selector: 'app-tecnico-update',
+  templateUrl: './tecnico-update.component.html',
+  styleUrls: ['./tecnico-update.component.css']
 })
-export class TecnicosUpdateComponent implements OnInit {
+export class TecnicoUpdateComponent implements OnInit {
 
   tecnico: Tecnico = {
     id:         '',
@@ -22,7 +23,6 @@ export class TecnicosUpdateComponent implements OnInit {
     dataCriacao: ''
   }
 
-  //Controler de validação do campos do formularios
   nome: FormControl =  new FormControl(null, Validators.minLength(3));
   cpf: FormControl =       new FormControl(null, Validators.required);
   email: FormControl =        new FormControl(null, Validators.email);
@@ -30,16 +30,16 @@ export class TecnicosUpdateComponent implements OnInit {
 
   constructor(
     private service: TecnicoService,
-    private toast: ToastrService,
-    private router : Router,
-    private routeHttpActivate : ActivatedRoute
-  ) { }
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
-    this.tecnico.id = this.routeHttpActivate.snapshot.paramMap.get('id')
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
     this.findById();
-  }
-  //Método para encontrar por id
+   }
+
   findById(): void {
     this.service.findById(this.tecnico.id).subscribe(resposta => {
       resposta.perfis = []
@@ -47,35 +47,30 @@ export class TecnicosUpdateComponent implements OnInit {
     })
   }
 
-  //Método de Atualizar
   update(): void {
-    this.service.update(this.tecnico).subscribe( () =>{
+    this.service.update(this.tecnico).subscribe(() => {
       this.toast.success('Técnico atualizado com sucesso', 'Update');
       this.router.navigate(['tecnicos'])
-    },
-    //Capturar error
-    excessCatch => {
-      if(excessCatch.error.errors) {
-        excessCatch.error.errors.forEach(element => {
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
           this.toast.error(element.message);
         });
       } else {
-        this.toast.error(excessCatch.error.message);
+        this.toast.error(ex.error.message);
       }
     })
   }
 
-  //Add profiles (roles)
   addPerfil(perfil: any): void {
-    //Logica para não repetir os profiles
     if(this.tecnico.perfis.includes(perfil)) {
       this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
     } else {
       this.tecnico.perfis.push(perfil);
     }
+    
   }
   
-  //Método para Validar no html de input
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid
      && this.email.valid && this.senha.valid
